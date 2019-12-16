ace7ccc70739560969d16856ccc6a52f81b935d3
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-12-16 16:07:03 -0500

    works page added to nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import MainPage from './components/MainPage'
+import WorkPage from './components/WorkPage'
 import { Menu } from 'semantic-ui-react'
 import {Route, Switch, Link, Redirect} from 'react-router-dom';
 
@@ -23,14 +24,18 @@ class App extends React.Component {
       <React.Fragment>
         <Menu stackable>
           <Link to='/home'>
-            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}>
-               Home
-             </Menu.Item>
-           </Link>
+            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}> Home </Menu.Item>
+          </Link>
+          <Link to='/works'>
+            <Menu.Item name='works' active={activeItem === 'works'} onClick={this.handleItemClick}> Works </Menu.Item>
+          </Link>
+
         </Menu>
       <div className="App">
         <switch>
         <Route exact path="/" render={() => <MainPage />} />
+        <Route exact path="/home" render={() => <MainPage />} />
+        <Route exact path="/works" render={() => <WorkPage />} />
         </switch>
       </div>
       </React.Fragment>
