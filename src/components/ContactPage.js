03396a628e81404d4df5cb1086f642d9e74001f2
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-12-19 17:12:17 -0500

    contact information page header editted

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { List, Divider } from 'semantic-ui-react'
+
 
 class ContactPage extends React.Component {
 
   render(){
     return(
       <div>
-      <p> This is the Contact page !</p>
+      <h2> Contact Information</h2>
+      <br></br>
+      <List>
+        <List.Item icon='users' content='Semantic UI' />
+        <Divider />
+        <List.Item icon='marker' content='Laurel, MD' />
+        <Divider />
+        <List.Item icon='mail' content={<a href='mailto:jack@semantic-ui.com'>jack@semantic-ui.com</a>} />
+        <Divider />
+        <List.Item icon='linkify' content={<a href='http://www.semantic-ui.com'>semantic-ui.com</a>} />
+      </List>
       </div>
     )
   }
